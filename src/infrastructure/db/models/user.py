8c02f4373b0025907d34a0a301c5ea62c6bba110
edpyt <edrsa1321s@gmail.com>
2023-11-03 20:02:16 +0300

    feat(game): update game route

--- src/infrastructure/db/models/user.py
+++ src/infrastructure/db/models/user.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from beanie import Document, Indexed
+from beanie import Document, Indexed, PydanticObjectId
 from fastapi_users.db import BeanieBaseUser
 
 
@@ -10,5 +10,20 @@ class User(BeanieBaseUser, Document):
 
     @classmethod
     async def get_by_username(cls, username: str) -> Optional["User"]:
-        """Get a single user by username"""
+        """
+        Get a single user by username
+
+        Params:
+            username(str): Unique username field
+        """
         return await cls.find_one(cls.username == username)
+
+    @classmethod
+    async def get_by_id(cls, id_: PydanticObjectId) -> Optional["User"]:
+        """
+        Get by MongoDB id
+
+        Params:
+            id_(PydanticObjectId): MongoDB document ObjectID "_id" field.
+        """
+        return await cls.find_one(cls.id == id_)
